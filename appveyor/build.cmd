628ef67046b33e468f5c1df1a9605f5e4da8aaca
Fernando Herrero <alphp@users.noreply.github.com>
2017-01-07 00:56:25 +0100

    Add files via upload

--- appveyor/build.cmd
+++ appveyor/build.cmd
@@ -1,124 +1,50 @@
 setlocal enableextensions enabledelayedexpansion
 	cd C:\projects
 
-	set ARCHITECTURES=amd64 x86
+	wget http://windows.php.net/downloads/php-sdk/php-sdk-binary-tools-20110915.zip
+	7z x -y php-sdk-binary-tools-20110915.zip -oC:\projects\php-sdk
+
+	git clone -q --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
+
+	mkdir C:\projects\php-src\ext\win32service
+	xcopy C:\projects\win32service C:\projects\php-src\ext\win32service /s /e /y
+
+	mkdir C:\projects\win32service\artifacts
+	xcopy C:\projects\win32service\*.php C:\projects\win32service\artifacts /y
+	set > C:\projects\win32service\artifacts\environment.txt
+
+rem	set ARCHITECTURES=amd64 x86
+rem	set ZTS_STATES=enable disable
+	set ARCHITECTURES=amd64
+	set ZTS_STATES=enable
+
 	for %%a in (%ARCHITECTURES%) do (
 		set ARCH=%%a
 		if "!ARCH!"=="amd64" set DEPTS_ARCH=x64
 		if "!ARCH!"=="x86" set DEPTS_ARCH=x86
-		echo %PHP_REL% !ARCH! !DEPTS_ARCH!
+
+		call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" %ARCH%
+		call C:\projects\php-sdk\bin\phpsdk_setvars.bat
+
+		wget http://windows.php.net/downloads/php-sdk/deps-%PHP_REL%-vc14-%DEPTS_ARCH%.7z
+		7z x -y deps-%PHP_REL%-vc14-%DEPTS_ARCH%.7z -oC:\projects\php-src
+
+		for %%z in (%ZTS_STATES%) do (
+			set ZTS_STATE=%%z
+			if "!ZTS_STATE!"=="enable" set ZTS_SHORT=ts
+			if "!ZTS_STATE!"=="disable" set ZTS_SHORT=nts
+
+			cd C:\projects\php-src
+			call buildconf.bat
+
+			rem call configure.bat --disable-all --with-mp=auto --enable-cli --!ZTS_STATE!-zts --enable-win32service=shared --with-config-file-scan-dir=C:\projects\win32service\build\modules.d --with-prefix=C:\projects\win32service\build --with-php-build=deps
+			call configure.bat --disable-all --with-mp=auto --enable-cli --!ZTS_STATE!-zts --enable-win32service=shared
+
+			nmake
+			nmake install
+
+			move %APPVEYOR_BUILD_FOLDER%\build\ext\php_win32service.dll artifacts\php_win32service-%PHP_REL%-vc14-!ZTS_SHORT!-!DEPTS_ARCH!.dll
+		)
 	)
-rem	set
-goto end
 
-    wget http://windows.php.net/downloads/php-sdk/php-sdk-binary-tools-20110915.zip
-
-    7z x -y php-sdk-binary-tools-20110915.zip -oC:\projects\php-sdk
-
-    git clone -q --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
-
-    mkdir C:\projects\php-src\ext\win32service
-
-    xcopy C:\projects\win32service C:\projects\php-src\ext\win32service /s /e /y
-
-    mkdir C:\projects\win32service\artifacts
-
-    xcopy C:\projects\win32service\*.php C:\projects\win32service\artifacts /y
-
-    set ARCH=amd64
-
-    set DEPTS_ARCH=x64
-
-    "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" %ARCH%
-
-    C:\projects\php-sdk\bin\phpsdk_setvars.bat
-
-    wget http://windows.php.net/downloads/php-sdk/deps-%PHP_REL%-vc14-%DEPTS_ARCH%.7z
-
-    7z x -y deps-%PHP_REL%-vc14-%DEPTS_ARCH%.7z -oC:\projects\php-src
-
-    cd C:\projects\php-src
-
-    set ZTS_STATE=enable
-
-    set ZTS_SHORT=ts
-
-    buildconf.bat
-
-    configure.bat --disable-all --with-mp=auto --enable-cli --%ZTS_STATE%-zts --enable-win32service=shared --with-config-file-scan-dir=C:\projects\win32service\build\modules.d --with-prefix=C:\projects\win32service\build --with-php-build=deps
-
-    nmake
-
-    nmake install
-
-    cd %APPVEYOR_BUILD_FOLDER%
-
-    move build\ext\php_win32service.dll artifacts\php_win32service-%PHP_REL%-vc14-%ZTS_SHORT%-%DEPTS_ARCH%.dll
-
-    cd C:\projects\php-src
-
-    set ZTS_STATE=disable
-
-    set ZTS_SHORT=nts
-
-    buildconf.bat
-
-    configure.bat --disable-all --with-mp=auto --enable-cli --%ZTS_STATE%-zts --enable-win32service=shared --with-config-file-scan-dir=C:\projects\win32service\build\modules.d --with-prefix=C:\projects\win32service\build --with-php-build=deps
-
-    nmake
-
-    nmake install
-
-    cd %APPVEYOR_BUILD_FOLDER%
-
-    move build\ext\php_win32service.dll artifacts\php_win32service-%PHP_REL%-vc14-%ZTS_SHORT%-%DEPTS_ARCH%.dll
-
-    set ARCH=x86
-
-    set DEPTS_ARCH=x86
-
-    "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" %ARCH%
-
-    C:\projects\php-sdk\bin\phpsdk_setvars.bat
-
-    wget http://windows.php.net/downloads/php-sdk/deps-%PHP_REL%-vc14-%DEPTS_ARCH%.7z
-
-    7z x -y deps-%PHP_REL%-vc14-%DEPTS_ARCH%.7z -oC:\projects\php-src
-
-    cd C:\projects\php-src
-
-    set ZTS_STATE=enable
-
-    set ZTS_SHORT=ts
-
-    buildconf.bat
-
-    configure.bat --disable-all --with-mp=auto --enable-cli --%ZTS_STATE%-zts --enable-win32service=shared --with-config-file-scan-dir=C:\projects\win32service\build\modules.d --with-prefix=C:\projects\win32service\build --with-php-build=deps
-
-    nmake
-
-    nmake install
-
-    cd %APPVEYOR_BUILD_FOLDER%
-
-    move build\ext\php_win32service.dll artifacts\php_win32service-%PHP_REL%-vc14-%ZTS_SHORT%-%DEPTS_ARCH%.dll
-
-    cd C:\projects\php-src
-
-    set ZTS_STATE=disable
-
-    set ZTS_SHORT=nts
-
-    buildconf.bat
-
-    configure.bat --disable-all --with-mp=auto --enable-cli --%ZTS_STATE%-zts --enable-win32service=shared --with-config-file-scan-dir=C:\projects\win32service\build\modules.d --with-prefix=C:\projects\win32service\build --with-php-build=deps
-
-    nmake
-
-    nmake install
-
-    cd %APPVEYOR_BUILD_FOLDER%
-
-    move build\ext\php_win32service.dll artifacts\php_win32service-%PHP_REL%-vc14-%ZTS_SHORT%-%DEPTS_ARCH%.dll
-:end
 endlocal
