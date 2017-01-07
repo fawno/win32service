bbd8494227a4a064349a4f5d192560b562f1e4a9
Fernando Herrero <alphp@users.noreply.github.com>
2017-01-07 01:48:14 +0100

    Add files via upload

--- appveyor/build.cmd
+++ appveyor/build.cmd
@@ -1,3 +1,4 @@
+@echo on
 setlocal enableextensions enabledelayedexpansion
 	cd C:\projects
 
@@ -11,29 +12,34 @@ setlocal enableextensions enabledelayedexpansion
 
 	mkdir C:\projects\win32service\artifacts
 	xcopy C:\projects\win32service\*.php C:\projects\win32service\artifacts /y
-	set > C:\projects\win32service\artifacts\environment.txt
 
-	set ARCH=amd64
-	set DEPTS_ARCH=x64
+	for %%a in (%ARCHITECTURES%) do (
+		set ARCH=%%a
+		if "!ARCH!"=="amd64" set DEPTS_ARCH=x64
+		if "!ARCH!"=="x86" set DEPTS_ARCH=x86
 
-	call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" %ARCH%
-	call C:\projects\php-sdk\bin\phpsdk_setvars.bat
+		call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" !ARCH!
+		call C:\projects\php-sdk\bin\phpsdk_setvars.bat
 
-	wget http://windows.php.net/downloads/php-sdk/deps-%PHP_REL%-vc14-%DEPTS_ARCH%.7z
-	7z x -y deps-%PHP_REL%-vc14-%DEPTS_ARCH%.7z -oC:\projects\php-src
+		wget http://windows.php.net/downloads/php-sdk/deps-%PHP_REL%-vc14-!DEPTS_ARCH!.7z
+		7z x -y deps-%PHP_REL%-vc14-%DEPTS_ARCH%.7z -oC:\projects\php-src
 
-	set ZTS_STATE=enable
-	set ZTS_SHORT=ts
+		for %%z in (%ZTS_STATES%) do (
+			set ZTS_STATE=%%z
+			if "!ZTS_STATE!"=="enable" set ZTS_SHORT=ts
+			if "!ZTS_STATE!"=="disable" set ZTS_SHORT=nts
 
-	cd C:\projects\php-src
-	call buildconf.bat
-	call configure.bat --disable-all --with-mp=auto --enable-cli --%ZTS_STATE%-zts --enable-win32service=shared --with-config-file-scan-dir=C:\projects\win32service\build\modules.d --with-prefix=C:\projects\win32service\build --with-php-build=deps
+echo			cd C:\projects\php-src
+echo			call buildconf.bat
+echo			call configure.bat --disable-all --with-mp=auto --enable-cli --!ZTS_STATE!-zts --enable-win32service=shared --with-config-file-scan-dir=C:\projects\win32service\build\modules.d --with-prefix=C:\projects\win32service\build --with-php-build=deps
 
-	nmake
-	nmake install
+echo			nmake
+echo			nmake install
 
-	cd %APPVEYOR_BUILD_FOLDER%
-	move build\ext\php_win32service.dll artifacts\php_win32service-%PHP_REL%-vc14-%ZTS_SHORT%-%DEPTS_ARCH%.dll
+echo			cd %APPVEYOR_BUILD_FOLDER%
+echo			move build\ext\php_win32service.dll artifacts\php_win32service-%PHP_REL%-vc14-!ZTS_SHORT!-!DEPTS_ARCH!.dll
+		)
+	)
 
 goto end
 	cd C:\projects
