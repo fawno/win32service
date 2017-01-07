7d8970b6cb0107106f8b431c17f0d22b402ab4ad
Fernando Herrero <alphp@users.noreply.github.com>
2017-01-07 03:16:06 +0100

    Add files via upload

--- appveyor/build.cmd
+++ appveyor/build.cmd
@@ -1,15 +1,12 @@
-@echo on
+@echo off
 setlocal enableextensions enabledelayedexpansion
 	set PHP_INI_SCAN_DIR=%APPVEYOR_BUILD_FOLDER%\build\modules.d
 
-	@if not exist c:\appveyor\. (
-		mkdir c:\appveyor
-	)
-
-	cd c:\appveyor
+	cd %APPVEYOR_BUILD_FOLDER%\appveyor
 	wget -N --progress=bar:force:noscroll http://windows.php.net/downloads/php-sdk/php-sdk-binary-tools-20110915.zip
 	7z x -y php-sdk-binary-tools-20110915.zip -oC:\projects\php-sdk
 
+	echo git clone -q --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
 	git clone -q --branch=PHP-%PHP_REL% https://github.com/php/php-src C:\projects\php-src
 
 	mkdir C:\projects\php-src\ext\win32service
@@ -18,7 +15,7 @@ setlocal enableextensions enabledelayedexpansion
 	mkdir %APPVEYOR_BUILD_FOLDER%\artifacts
 	xcopy %APPVEYOR_BUILD_FOLDER%\*.php %APPVEYOR_BUILD_FOLDER%\artifacts /y
 
-	@for %%a in (%ARCHITECTURES%) do (
+	for %%a in (%ARCHITECTURES%) do (
 		set ARCH=%%a
 		if "!ARCH!"=="amd64" set DEPTS_ARCH=x64
 		if "!ARCH!"=="x86" set DEPTS_ARCH=x86
@@ -26,11 +23,11 @@ setlocal enableextensions enabledelayedexpansion
 		call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" !ARCH!
 		call C:\projects\php-sdk\bin\phpsdk_setvars.bat
 
-		cd c:\appveyor
+		cd %APPVEYOR_BUILD_FOLDER%\appveyor
 		wget -N --progress=bar:force:noscroll http://windows.php.net/downloads/php-sdk/deps-%PHP_REL%-vc14-!DEPTS_ARCH!.7z
 		7z x -y deps-%PHP_REL%-vc14-!DEPTS_ARCH!.7z -oC:\projects\php-src
 
-		@for %%z in (%ZTS_STATES%) do (
+		for %%z in (%ZTS_STATES%) do (
 			set ZTS_STATE=%%z
 			if "!ZTS_STATE!"=="enable" set ZTS_SHORT=ts
 			if "!ZTS_STATE!"=="disable" set ZTS_SHORT=nts
@@ -43,9 +40,7 @@ setlocal enableextensions enabledelayedexpansion
 			nmake install
 
 			cd %APPVEYOR_BUILD_FOLDER%
-			copy build\ext\php_win32service.dll artifacts\php_win32service-%PHP_REL%-vc14-!ZTS_SHORT!-!DEPTS_ARCH!.dll
-			cd build
-			php -i > ..\artifacts\php_win32service-%PHP_REL%-vc14-!ZTS_SHORT!-!DEPTS_ARCH!.txt
+			move build\ext\php_win32service.dll artifacts\php_win32service-%PHP_REL%-vc14-!ZTS_SHORT!-!DEPTS_ARCH!.dll
 		)
 	)
 endlocal
